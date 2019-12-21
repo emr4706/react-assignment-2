d8afa0341077c190f36f88fa502c3b1882d1f2e8
emr4706 <emr4706@gmail.com>
2019-12-21 10:56:28 +0100

    add form

--- src/Components/Home/Content.js
+++ src/Components/Home/Content.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { makeStyles } from "@material-ui/core/styles";
 import List from "@material-ui/core/List";
 import ListItem from "@material-ui/core/ListItem";
@@ -7,10 +7,9 @@ import ListItemText from "@material-ui/core/ListItemText";
 import ListItemAvatar from "@material-ui/core/ListItemAvatar";
 import Avatar from "@material-ui/core/Avatar";
 import Typography from "@material-ui/core/Typography";
-import { database } from '../../data';
+import { database } from "../../data";
 
 console.log(database);
-
 
 const useStyles = makeStyles(theme => ({
   root: {
@@ -24,42 +23,73 @@ const useStyles = makeStyles(theme => ({
 }));
 
 export default function Content() {
-    
+  
   const classes = useStyles();
+  const [user, setUser] = useState({
+    userName: "",
+    userComment: ""
+  })
+
+  function handlePost(event) {
+    const {name, value} = event.target;
+    setUser(prevValue => {
+      return {
+        ...prevValue,
+        [name]: value
+      }
+    })
+    event.preventDefault();
+  }
 
   return (
-    <List className={classes.root}>
-      {database.map((data, index) => (
-        <React.Fragment key={index}>
-         <ListItem alignItems="flex-start" > 
-        <ListItemAvatar>
-          <Avatar
-            alt="Remy Sharp"
-            src="https://picsum.photos/200/300?random=1"
-          />
-        </ListItemAvatar>
-        <ListItemText
-          primary={data.comment}
-          secondary={
-            <React.Fragment>
-              <Typography
-                component="span"
-                variant="body2"
-                className={classes.inline}
-                color="textPrimary"
-              >
-            {data.name}
-              </Typography>
-              
-            </React.Fragment>
-          }
+    <div className="container">
+      <form onSubmit={handlePost}>
+        <input
+        onChange={handlePost}
+         name="userName" 
+         placeholder="Username"
+         value={user.userName}
+         />
+        <input
+        onChange={handlePost} 
+        name="userComment" 
+        placeholder="What do you think?"
+        value={user.userComment}
         />
-      </ListItem>
-      <Divider variant="inset" component="li" /> 
-        </React.Fragment>
-        
-      ))}
-      
-    </List>
+        <button>Submit</button>
+      </form>
+      <p>{user.userName}</p>
+      <p>{user.userComment}</p>
+      <List className={classes.root}>
+        {database.map((data, index) => (
+          <React.Fragment key={index}>
+            <ListItem alignItems="flex-start">
+              <ListItemAvatar>
+                <Avatar
+                  alt="Remy Sharp"
+                  src="https://picsum.photos/200/300?random=1"
+                />
+              </ListItemAvatar>
+              <ListItemText
+                primary={data.comment}
+                secondary={
+                  <React.Fragment>
+                    <Typography
+                      component="span"
+                      variant="body2"
+                      className={classes.inline}
+                      color="textPrimary"
+                    >
+                      {data.name}
+                    </Typography>
+                  </React.Fragment>
+                }
+              />
+            </ListItem>
+            <Divider variant="inset" component="li" />
+          </React.Fragment>
+        ))}
+      </List>
+    </div>
   );
 }
--- src/Components/Home/Navbar.js
+++ src/Components/Home/Navbar.js
@@ -1,38 +1,61 @@
-import React from 'react';
-import { makeStyles } from '@material-ui/core/styles';
-import AppBar from '@material-ui/core/AppBar';
-import Toolbar from '@material-ui/core/Toolbar';
-import Typography from '@material-ui/core/Typography';
-import Button from '@material-ui/core/Button';
-import IconButton from '@material-ui/core/IconButton';
-import MenuIcon from '@material-ui/icons/Menu';
+import React, { useState } from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import AppBar from "@material-ui/core/AppBar";
+import Toolbar from "@material-ui/core/Toolbar";
+import Typography from "@material-ui/core/Typography";
+import Button from "@material-ui/core/Button";
+import IconButton from "@material-ui/core/IconButton";
+import MenuIcon from "@material-ui/icons/Menu";
 
 const useStyles = makeStyles(theme => ({
   root: {
-    flexGrow: 1,
+    flexGrow: 1
   },
   menuButton: {
-    marginRight: theme.spacing(2),
+    marginRight: theme.spacing(2)
   },
   title: {
-    flexGrow: 1,
-  },
+    flexGrow: 1
+  }
 }));
 
 export default function Navbar() {
   const classes = useStyles();
 
+  const [Inlogd, setInlogd] = useState(true);
+
+  const login = () => {
+    setInlogd(false);
+  };
+
+  const logout = () => {
+    setInlogd(true);
+  };
+
   return (
     <div className={classes.root}>
       <AppBar position="static">
         <Toolbar>
-          <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
+          <IconButton
+            edge="start"
+            className={classes.menuButton}
+            color="inherit"
+            aria-label="menu"
+          >
             <MenuIcon />
           </IconButton>
           <Typography variant="h6" className={classes.title}>
             News
           </Typography>
-          <Button color="inherit">Login</Button>
+          {Inlogd === true ? (
+            <Button color="inherit" onClick={() => logout()}>
+              Logout
+            </Button>
+          ) : (
+              <Button color="inherit" onclick={() => login()}>
+                Login
+            </Button>
+            )}
         </Toolbar>
       </AppBar>
     </div>
